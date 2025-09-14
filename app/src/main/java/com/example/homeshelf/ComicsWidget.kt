6a575db92b81efc8fb3e365fd21e2df37072210b
kegra <nukegarara@yahoo.co.jp>
2025-09-14 14:30:40 +0900

    widget button event

--- app/src/main/java/com/example/homeshelf/ComicsWidget.kt
+++ app/src/main/java/com/example/homeshelf/ComicsWidget.kt
@@ -5,10 +5,10 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import android.widget.ArrayAdapter
+import android.util.Log
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
+import androidx.core.net.toUri
 
 
 class ListWidgetService : RemoteViewsService() {
@@ -79,10 +79,26 @@ class ListRemoteViewsFactory(
     }
 }
 
+const val ACTION_LEFTBUTTON = "WIDGET_LEFTBUTTON_CLICKED"
+const val ACTION_RIGHTBUTTON = "WIDGET_RIGHTBUTTON_CLICKED"
+
 /**
  * Implementation of App Widget functionality.
  */
 class ComicsWidget : AppWidgetProvider() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if(intent == null)
+            return
+
+        if (intent.action == ACTION_LEFTBUTTON) {
+            Log.i("MyApp", "leftbutton")
+        }
+        if (intent.action == ACTION_RIGHTBUTTON) {
+            Log.i("MyApp", "rightbutton")
+        }
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -90,55 +106,32 @@ class ComicsWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            val platformVersion = arrayOf<String>(
-                "Android 10.0",
-                "Android 9",
-                "Android 8.1",
-                "Android 8.0",
-                "Android 7.1.1",
-                "Android 7.1",
-                "Android 7.0",
-                "Android 6.0",
-                "Android 5.1",
-                "Android 5.0",
-                "Android 4.4W",
-                "Android 4.4",
-                "Android 4.3",
-                "Android 4.2、4.2.2",
-                "Android 4.1、4.1.1",
-                "Android 4.0.3、4.0.4",
-                "Android 4.0、4.0.1、4.0.2",
-                "Android 3.2",
-                "Android 3.1.x",
-                "Android 3.0.x",
-                "Android 2.3.4",
-                "Android 2.3.3",
-                "Android 2.3.2",
-                "Android 2.3.1",
-                "Android 2.3",
-                "Android 2.2.x",
-                "Android 2.1.x",
-                "Android 2.0.1",
-                "Android 2.0",
-                "Android 1.6",
-                "Android 1.5",
-                "Android 1.1",
-                "Android 1.0",
-            )
-
-            val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(
-                context,
-                R.layout.widget_comiclist_item,
-                platformVersion
-            )
-
             // Set up the intent that starts the StackViewService, which
             // provides the views for this collection.
             val intent = Intent(context, ListWidgetService::class.java).apply {
                 // Add the widget ID to the intent extras.
                 putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-                data = Uri.parse(toUri(Intent.URI_INTENT_SCHEME))
+                data = toUri(Intent.URI_INTENT_SCHEME).toUri()
             }
+
+            val leftButtonIntent = Intent(context, ComicsWidget::class.java).apply {
+                action = ACTION_LEFTBUTTON
+            }
+            val leftButtonPendingIntent = PendingIntent.getBroadcast(
+                context,
+                0,
+                leftButtonIntent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+            val rightButtonIntent = Intent(context, ComicsWidget::class.java).apply {
+                action = ACTION_RIGHTBUTTON
+            }
+            val rightButtonPendingIntent = PendingIntent.getBroadcast(
+                context,
+                0,
+                rightButtonIntent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
             // Instantiate the RemoteViews object for the widget layout.
             val views = RemoteViews(context.packageName, R.layout.comics_widget).apply {
                 // Set up the RemoteViews object to use a RemoteViews adapter.
@@ -153,7 +146,10 @@ class ComicsWidget : AppWidgetProvider() {
                 setEmptyView(R.id.appwidget_itemlist, R.id.appwidget_empty_view)
 
                 setImageViewResource(R.id.widget_button_left, R.drawable.ic_button_left)
+                setOnClickPendingIntent(R.id.widget_button_left, leftButtonPendingIntent)
+
                 setImageViewResource(R.id.widget_button_right, R.drawable.ic_button_right)
+                setOnClickPendingIntent(R.id.widget_button_right, rightButtonPendingIntent)
             }
 
             appWidgetManager.updateAppWidget(appWidgetId, views)
@@ -168,26 +164,4 @@ class ComicsWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-}
-
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.comics_widget)
-//    views.setTextViewText(R.id.appwidget_text, widgetText)
-
-    // 空のPendingIntentを設定してデフォルト動作を上書き
-    val emptyIntent = Intent()
-    val pendingIntent = PendingIntent.getActivity(
-        context, 0, emptyIntent,
-        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-    )
-    views.setOnClickPendingIntent(android.R.id.background, pendingIntent)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
